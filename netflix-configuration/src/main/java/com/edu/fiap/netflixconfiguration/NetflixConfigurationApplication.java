64eb1bdeec12d88b3dbff0d96cd45dbf893fde7b
guina.costa <agnaldo.almeida@iteris.com.br>
2019-04-04 20:09:00 -0300

    discovery server and configuration server

---
+++ netflix-configuration/src/main/java/com/edu/fiap/netflixconfiguration/NetflixConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fiap.netflixconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixConfigurationApplication.class, args);
+	}
+
+}
---
+++ netflix-discovery/src/main/java/com/edu/fiap/netflixdiscovery/NetflixDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fiap.netflixdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixDiscoveryApplication.class, args);
+	}
+
+}
