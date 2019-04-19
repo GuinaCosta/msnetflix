4b103b4686d385513cbcbdd61112700f634f4be0
LFrigoDeSouza <lfrigodesouza@outlook.com>
2019-04-19 01:49:16 -0300

    config server catalog-processing e catalog-search-service

--- catalog-search-service/src/main/java/com/edu/fiap/catalogsearchservice/CatalogSearchServiceApplication.java
+++ catalog-search-service/src/main/java/com/edu/fiap/catalogsearchservice/CatalogSearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.edu.fiap.catalogsearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class CatalogSearchServiceApplication {
 
 	public static void main(String[] args) {
