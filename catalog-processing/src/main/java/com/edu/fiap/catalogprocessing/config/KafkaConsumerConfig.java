25ff69fc2a2c386bbe012c03365bebff4c3574ef
guina.costa <agnaldo.almeida@iteris.com.br>
2019-04-29 22:45:52 -0300

    Configurações para conseguir obter as mensagens do kafka e deserializar

--- catalog-processing/src/main/java/com/edu/fiap/catalogprocessing/config/KafkaConsumerConfig.java
+++ catalog-processing/src/main/java/com/edu/fiap/catalogprocessing/config/KafkaConsumerConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -18,47 +20,40 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
-
-    /**
-     * Kafka bootstrap address
-     */
     @Value(value = "${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
-    /**
-     * Kafka consumer group.id
-     */
-    @Value(value = "${spring.kafka.consumer.group-id}")
-    private String consumerGroupId;
+    @Value("$spring.kafka.consumer.group-id")
+    private String groupId;
 
-    /**
-     * Kafka Consumer Factory Bean
-     * @return Consumer Factory
-     */
     @Bean
-    public ConsumerFactory<String, CatalogProcessingRequest> consumerFactory() {
+    @SuppressWarnings("Duplicates")
+    public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 
-        return new DefaultKafkaConsumerFactory<>(props,
-                new StringDeserializer(),
-                new JsonDeserializer<>(CatalogProcessingRequest.class));
+        return props;
     }
 
-    /**
-     * Concurrent Kafka Listener Container Factory Bean
-     * @return Kafka Listener Container Factory
-     */
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, CatalogProcessingRequest> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, CatalogProcessingRequest> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+    public ConsumerFactory<String, CatalogProcessingRequest> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                consumerConfigs(),
+                new StringDeserializer(),
+                new JsonDeserializer<>(CatalogProcessingRequest.class, false));
+    }
 
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, CatalogProcessingRequest>>
+        kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CatalogProcessingRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-
         return factory;
     }
 }
